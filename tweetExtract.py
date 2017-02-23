1e24a80fcd3bee74dd51b71d050a36fb2f9115e0
Gloire Rubambiza <rubambig@mail.gvsu.edu>
2017-02-22 21:55:37 -0500

    Extracts attributes requested in 2/22/17 meeting
    It extracts the user id string, the time stamp of the tweet, the text, the location of the user, and whether the tweet was a retweet
    
    A few more attributes were extracted and hidden just in case of future needs

--- tweetExtract.py
+++ tweetExtract.py
@@ -3,34 +3,42 @@ import json
 
 data_file = open('OneTweet.txt') #opening the file
 json_str = data_file.read() #read the file into a single string
-
-# able to handle one json object but
-# throws an "Extra data" valueError when
-# attempting to load more than one object
 json_dicts = json.loads(json_str) 
-#print (type(json_dicts))
 
-user_name = json_dicts['user']['screen_name'] #extract the screen_name
-tweet_time = json_dicts['created_at'] #extract the date
-followers = json_dicts['user']['followers_count'] #extract the user's followers
-friends = json_dicts['user']['friends_count']
+#extracting tweet and user information
+user_id_str = json_dicts['user']['id_str']
+tweet_time = json_dicts['created_at'] 
 tweet = json_dicts['text'] #extract the tweet
+retweet_status = json_dicts['is_quote_status']
+tweet_id_str = json_dicts['id_str']
+location = ""
+if(json_dicts['user']['geo_enabled'] ):
+   location = json_dicts['user']['location']
+else:
+   location = 'global'
+
+#all fields below are extracted just in case of future needs
+#followers = json_dicst['user']['followers_count']
+#friends = json_dicst['user']['friends_count']
+#user_name = json_dicts['user']['screen_name']
 
 #tried extracting full tweet, still getting the keys wrong
-#tweet = json_dicts['retweeted_status']['extended_text']['full_text']
+#tweet = json_dicts['entities']['extended_text']['full_text']
 
 tweet_object = {
-                "full_tweet":tweet, 
-		"followers":followers, 
-		"friends": friends
+		"tweet_id": tweet_id_str,
+		"created_at": tweet_time,
+		"location": location,
+                "tweet": tweet, 
+	#	"followers":followers, 
+	#	"friends": friends,
+		"is_retweet": retweet_status
 }
 tweet_summary = {
-		"screen_name": user_name, 
+		"user_id_str" : user_id_str, 
 		"tweet":tweet_object
 		}
 
-#user_tweets[user_name] = tweet
-#print (user_tweets)
 print(tweet_summary)
 
 
